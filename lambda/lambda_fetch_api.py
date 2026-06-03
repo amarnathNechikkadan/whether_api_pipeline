37eddf20845791330ed39c649da19a589c412a5d
amarnathNechikkadan <amarnathnk567@gmail.com>
2026-06-03 15:10:28 +0530

    Add DynamoDB table configuration from environment variables

--- lambda/lambda_fetch_api.py
+++ lambda/lambda_fetch_api.py
@@ -8,3 +8,6 @@ from decimal import Decimal
 
 # Connect to DynamoDB
 dynamodb = boto3.resource("dynamodb")
+
+# Get table name from environment variable
+table = dynamodb.Table(os.environ["DYNAMODB_TABLE"])
